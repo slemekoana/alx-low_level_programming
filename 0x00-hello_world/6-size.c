888655b5dc439e2c170309755dc30e944dfb4a03
Selaelo <selaelolemekoana@gmail.com>
2022-07-04 19:41:08 -0700

    Creating a c file that prints various size types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,20 @@
  *
  * Return: Always 0 (Success)
  *
- * */
+ */
 
 int main(void)
 {
-
+	int num;
+	long int dec;
+	long long int dou;
+	char chr;
+	float fl;
+	
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(chr));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(num));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(dec));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(dou));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(fl));
 	return (0);
 }
