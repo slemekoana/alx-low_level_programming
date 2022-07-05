3d5a0a140dd5c46bce611c96883fdcd6195c29ec
Selaelo <selaelolemekoana@gmail.com>
2022-07-04 19:07:45 -0700

    Print alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char chr;
+
+	for (chr = 'z'; chr >= 'a'; chr--)
+	{
+		putchar(chr);
+	}
+	putchar('\n');
+
+	return (0);
+}
