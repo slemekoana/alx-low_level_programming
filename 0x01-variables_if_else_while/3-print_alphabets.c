9078f44d7c4e963dce9149082aee62bcc3c073c6
Selaelo <selaelolemekoana@gmail.com>
2022-07-04 18:34:22 -0700

    Print alphabet in lower and uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char chr;
+
+	for (chr = 'a'; chr <= 'z'; chr++)
+	{
+		putchar(chr);
+	}
+	for (chr = 'A'; chr <= 'Z'; chr++)
+	{
+		putchar(chr);
+	}
+	putchar('\n');
+
+	return (0);
+}
