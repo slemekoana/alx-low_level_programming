28dd802f76452c1f0db039ecd42ca6039e08d3cd
Selaelo <selaelolemekoana@gmail.com>
2022-07-05 17:42:37 -0700

    print a char

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "_putchar.c"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char c = "_putchar";
+_putchar(c);
+return (0);
+}
